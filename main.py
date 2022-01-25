0d4b9c8cffe3179b36378571ee8bdf4ae10d4c78
alvinli04 <alvin.li.j@gmail.com>
2022-01-24 22:53:49 -0500

    improved 24 game

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
+from utils import *
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -64,15 +65,17 @@ async def maketeams(ctx, n: int, *people):
 
 @bot.command(name='24')
 async def twentyfour(ctx):
-    nums = [random.randint(1,10) for _ in range(4)]
+    nums = [random.randint(1,9) for _ in range(4)]
     await ctx.send("Your numbers are: " + ', '.join([str(i) for i in nums]))
     
     chk = lambda m : m.author == ctx.author and m.channel == ctx.channel
     
+    ans = solve24([str(n) for n in nums])
+
     try:
         sol = await bot.wait_for("message", check=chk, timeout=40)
     except asyncio.TimeoutError:
-        await ctx.send(f"Time's up, {ctx.author.display_name}. I'm not waiting 2 years.")
+        await ctx.send(f"Time's up, {ctx.author.display_name}. I'm not waiting 2 years. A possible solution was " + ans + ".")
         return
 
     def sanitize(s, wl = '0123456789/*+-()'):
@@ -82,7 +85,7 @@ async def twentyfour(ctx):
     if sorted(nums) == sorted([int(c) for c in sol if c.isnumeric()]) and eval(sol) == 24:
         await ctx.send(f"Good job, {ctx.author.display_name}. Have a cookie :cookie:")
     else:
-        await ctx.send(f"{ctx.author.display_name}, that's wrong kid. Don't you know how to count?")
+        await ctx.send(f"{ctx.author.display_name}, that's wrong kid. Don't you know how to count? A possible solutin was " + ans + ".")
 
 
 @bot.command(name='wordle')
